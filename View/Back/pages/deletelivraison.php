eabe6d0d4802969630d5ac8b006cad490b5af1d0
nawara222 <101025483+nawara222@users.noreply.github.com>
2022-12-11 12:22:29 +0100

    Delete View directory

--- View/Back/pages/deletelivraison.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include '../../../config.php';
-include '../../../Controller/livraisonC.php';
-$livraisonC =new livraisonC();
-$livraisonC->supprimerlivraison($_GET["id_livraison"]);
-
-header('Location: http://localhost/yummy/View/Back/pages/listlivraison.php');
-
-?>
--- View/Back/pages/deletelivreur.php
+++
@@ -1,9 +0,0 @@
-<?php 
-include '../../../config.php';
-include '../../../Controller/livreurC.php';
-$livreurC =new livreurC();
-$livreurC->supprimerlivreur($_GET["id_livreur"]);
-
-header('Location: http://localhost/yummy/View/Back/pages/listlivreur.php');
-
-?>
